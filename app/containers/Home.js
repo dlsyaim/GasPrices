5598c96a0940e1eaebf42b28ac27961cead7fed1
ElisaV <elisav12@ru.is>
2017-02-11 15:18:26 +0000

    Buttons, style work

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -5,7 +5,8 @@ import {
 	View,
 	Text,
 	ActivityIndicator,
-	TouchableHighlight
+	TouchableHighlight,
+	StyleSheet
 } from 'react-native'
 import { fetchGasPrices } from '../actions/gas' 
 
@@ -22,17 +23,21 @@ class Home extends Component {
         });
 	}
 
+	// Returns first 10 gas stations in array to display
+	// Todo: add filter
 	mapGasStations(){
-		return Object.keys(this.props.gasPrices.data).map(key => this.props.gasPrices.data[key])
+		return Object.keys(this.props.gasPrices.data).map(key => this.props.gasPrices.data[key]).slice(0,10)
 	}
 
 	render() {
 		return(
-			<View>
-				<View>
-					<Text>Bensínstöðvar</Text>
+			<View style={styles.scene}>
+				<View style={styles.topBar}>
+					<TouchableHighlight style={styles.button} onPress={ console.log("click") }><Text style={styles.buttonText}>Vegalengd</Text></TouchableHighlight>
+					<TouchableHighlight style={styles.button} onPress={ console.log("click") }><Text style={styles.buttonText}>Flokka eftir</Text></TouchableHighlight>
+					<TouchableHighlight style={styles.button} onPress={ console.log("click") }><Text style={styles.buttonText}>Afsláttarkort</Text></TouchableHighlight>
 				</View>
-				<View>
+				<ScrollView style={styles.scrollSection}>
 					{ this.state.fetching ? 
 						<ActivityIndicator		
 	                   		style={[{height: 80}]}		
@@ -41,13 +46,54 @@ class Home extends Component {
  	                 	: null
 					}
 					{ !this.state.fetching && this.mapGasStations().map((result) => {
-						return <Text key={result.id} > {result.name}</Text>
+						return <View key={result.key} >
+							<Text style={styles.gasStationTitle}>{result.company} {result.name}</Text>
+							<Text style={styles.gasStationText}>Diesel: {result.diesel}</Text>
+							<Text style={styles.gasStationText}>Bensín: {result.bensin95}</Text>							
+						</View>
+
 					})}				
-				</View>
+				</ScrollView>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
+  topBar: {
+    height: 60,
+    flexDirection: 'row',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    padding: 5,
+  },
+  scrollSection: {
+    flex: 0.8
+  },
+  button: {
+    backgroundColor: '#AF98EF',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    margin: 5,
+    width: 100,
+    borderRadius: 10
+  },
+  buttonText: {
+    textAlign: 'center',
+  },
+  gasStationBox: {
+
+  },
+  gasStationTitle: {
+  	fontWeight: 'bold',
+  },
+  gasStationText: {
+
+  }
+});
 
 function mapStateToProps(state){
     return {
